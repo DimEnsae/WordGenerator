5e3ce9e3778879e3c396c2833c1623f375c7255b
DimEnsae <dimitri.meunier@ensae-paristech.fr>
2018-10-23 15:36:20 +0200

    Initial Commit

---
+++ WordGenerator/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  WordGenerator
+//
+//  Created by Dimitri Meunier on 23/10/2018.
+//  Copyright © 2018 Dimitri Meunier. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
