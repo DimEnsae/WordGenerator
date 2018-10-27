1bf0b229c856ddadb2f960c92b3931d931422add
DimEnsae <dimitri.meunier@ensae-paristech.fr>
2018-10-27 16:02:09 +0200

    Création d'une fonction de lecture d'un fichier mot a mot avec PATH relatif au projet

--- WordGenerator/main.cpp
+++ WordGenerator/main.cpp
@@ -7,11 +7,49 @@
 //
 
 #include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+int display_words (string name, vector<vector<int> > freq) {
+    
+    ifstream myFile(name);
+
+    if(myFile)
+    {
+        ifstream myFile(name);
+        int compteur=0;
+        string v;
+        while (myFile.peek() != EOF) {
+            myFile >> v;
+            cout << v[0] << endl;
+            compteur ++;
+        }
+        myFile.close();
+        cout << "Le nombre de mots est : " << compteur << endl;
+        
+        return compteur;
+    }
+    else
+    {
+        cout << "ERREUR: Impossible d'ouvrir le fichier." << endl;
+        return 42;
+    }
+    
+}
+
+
+
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Que la fête commence !\n";
-    int a=3;
-    std::cout << a << std::endl;
-    return 0;
+    cout << getenv("PWD") << endl;
+    
+    //Vecteurs de stockage des frequences
+    vector<vector<int> > freq(26, vector<int>(26));
+    cout << display_words("../pokemon_names.txt", freq);
+    
 }
+
+
+//Idea use structure to link letters to index of the matrixes
