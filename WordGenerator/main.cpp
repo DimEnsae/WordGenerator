4bae8a5f035dfcbfd3fa8d512e52d5ba5d0bfb01
DimEnsae <dimitri.meunier@ensae-paristech.fr>
2018-12-14 12:51:28 +0100

    last update

--- WordGenerator/main.cpp
+++ WordGenerator/main.cpp
@@ -3,13 +3,32 @@
 
 
 int main() {
-    corpus c("../data/NewEN.txt");
+    
+    
+     corpus c("../data/NewEN.txt");
+     std::cout << "Generation a l'ordre 1" << std::endl << std::endl;
+     c.genererUnMotOrdre1Bis(9,12);
+     std::cout << std::endl;
+     std::cout << "Generation a l'ordre 2" << std::endl << std::endl;
+     c.genererUnMotOrdre2Bis(9,12);
+     c.exportMatriceTocsv("./Mat.txt");
+     
+    
+    
+    /*
+     c.exportMatriceTocsv("./Mat.txt");
+     */
+
+    
+    /*
+    corpus c("../Mobydick.txt");
     std::cout << "Generation a l'ordre 1" << std::endl << std::endl;
     c.genererUnMotOrdre1Bis(9,12);
     std::cout << std::endl;
     std::cout << "Generation a l'ordre 2" << std::endl << std::endl;
     c.genererUnMotOrdre2Bis(9,12);
-    c.exportMatriceTocsv("./Mat.txt");
+*/
+    
     
     return 0;
 }
