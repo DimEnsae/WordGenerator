2140757f5d0300f514cf6fe2477878a67824abad
DimEnsae <dimitri.meunier@ensae-paristech.fr>
2018-12-18 09:52:25 +0100

    unfinished docstring automaton

--- WordGenerator/Automaton.cpp
+++ WordGenerator/Automaton.cpp
@@ -6,6 +6,7 @@
 #include <fstream>
 
 Automaton::Automaton(std::string path, int memoryLength){
+    
     std::map<std::string , Node> mp;
     this->mapNode = mp;
     this->memoryLength = memoryLength;
@@ -14,13 +15,10 @@ Automaton::Automaton(std::string path, int memoryLength){
     this->add_map("#", n1);
     this->add_map("$", n2);
 
-
-
     std::ifstream ifs(path);
     std::string v;
     ifs>>v;
     while (!ifs.eof()){
-     //std::cout<<v;
     this->learnFromWord(v);
     ifs>>v;
     }
@@ -29,30 +27,27 @@ Automaton::Automaton(std::string path, int memoryLength){
 }
 
 void Automaton::learnFromWord(std::string word){
+    
     int L = this->memoryLength;
     std::string noeud="#";
-   for(int i = 0 ;i<word.length();i++){
-       std::string noeud_suiv = word.substr (std::max(0,i-L+1), i - std::max(0,i-L+1) + 1);
-      // std::cout << "noeud : " << noeud << std::endl;
-
-       std::map<std::string, Node>::iterator it;
-       it = this->mapNode.find(noeud_suiv);
+    for(int i = 0 ;i<word.length();i++){
+        std::string noeud_suiv = word.substr (std::max(0,i-L+1), i - std::max(0,i-L+1) + 1);
+        
+        std::map<std::string, Node>::iterator it;
+        it = this->mapNode.find(noeud_suiv);
 
        if (it == this->mapNode.end()){
            Node n;
            this->add_map(noeud_suiv, n);
-        //   std::cout << "Je suis 1" << std::endl;
            }
 
        char followingLetter = noeud_suiv[noeud_suiv.length() - 1];
 
        if (this->mapNode[noeud].getMap().count(followingLetter)>0) {
            this->mapNode[noeud].increment(followingLetter);
-          // std::cout << "Je suis 2" << std::endl;
        }
        else{
            this->mapNode[noeud].add_map(followingLetter, Vertex(noeud_suiv, 1));
-         //std::cout << "Je suis 3" << std::endl;
   }
 
        //A BIEN REECRIRE
@@ -76,6 +71,7 @@ void Automaton::learnFromWord(std::string word){
        //std::cout << "following letter " << followingLetter << std::endl;
        //std::cout << "noeud_suiiv : " << noeud_suiv << std::endl;
    }
+    //A BIEN REECRIRE
     /*
     std::map<char, Vertex>::iterator it3;
     it3 = this->mapNode[noeud].getMap().find('$');
@@ -95,7 +91,7 @@ void Automaton::learnFromWord(std::string word){
 
 void Automaton::add_map(std::string idNode, Node node){
     this->mapNode.insert(std::pair<std::string,Node>(idNode,node));
-//this->mapVertex[next] =  v;
+    //this->mapVertex[next] =  v; ?????????
 }
 
 void Automaton::display(){
@@ -104,7 +100,7 @@ void Automaton::display(){
     for(it=this->mapNode.begin();it!=this->mapNode.end();it++){
 
     std::cout<<it->first<<":"<<std::endl;
-    it->second.display();//surcharger par la suite
+    it->second.display();
     std::cout<<std::endl;
     }
 }
@@ -118,10 +114,11 @@ void Automaton::generate_word(std::string noeud_suivant) {
       if (followingLetter != '$'){
         std::cout<<followingLetter;}
     }
+    
     if (noeud_suivant == "$") {std::cout<<std::endl;}
-    else{
-    noeud_suivant=this->mapNode[noeud_suivant].select_node_suivant();
-    //std::cout << followingLetter;
+    
+    else{noeud_suivant=this->mapNode[noeud_suivant].select_node_suivant();
+        
     generate_word(noeud_suivant);
   }
 }
--- WordGenerator/Automaton.h
+++ WordGenerator/Automaton.h
@@ -4,13 +4,37 @@
 #include <map>
 #include "Node.h"
 
+/*------------------------- AUTOMATON -------------------------------------
+ 
+Stock l'integralite du graphe contenu dans le corpus.
+ 
+ Arguments:
+    -map {next, Vertex}
+ -key, charactere avec lequel on sort du noeud, par exemple si on regarde le mot bol avec un memory length de 2 et qu'on est sur le noeud "bo" on sort avec le charactere 'l' qui nous ammene ou noeud "ol", la map contiendra donc {'l', ("ol", weight)}
+ -Values, Vertex associe a la sortiem dans l'exemple precedent: ("ol", weight)
+ 
+ Methodes:
+ -Un seul constructeur,
+    -memoryLength, ordre de la chaine de Markov
+    -path, adresse physique du corpus
+ -getMap, accesseur de la map
+ -add_map, ajout d'un element {next, Vertex} dans la map
+ 
+ -increment, appel la methode increment du Vertex associé au charactere de sortie
+ -display, affiche la map sous la forme
+ l => (ol,1)
+ 
+ --a completer
+ 
+ ----------------------------------------------------------------*/
+
 class Automaton{
 private:
-    int memoryLength; //memory of the Automaton
-    std::map<std::string,Node> mapNode;//the nodes
+    int memoryLength;
+    std::map<std::string,Node> mapNode;
 public:
     Automaton(std::string path, int memoryLength);
-    void learnFromWord(std::string word); //on généralisera aux mots plus tard
+    void learnFromWord(std::string word);
     void add_map(std::string idNode, Node node);
     void display();
     void generate_word(std::string);
